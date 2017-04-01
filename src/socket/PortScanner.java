260b8989f94936bd7faad36f775983541f9bd5e1
Chariemo <charleyschen@163.com>
2017-04-01 21:40:19 +0800

    multiThead to connect
    Signed-off-by: Chariemo <charleyschen@163.com>

--- src/socket/PortScanner.java
+++ src/socket/PortScanner.java
@@ -5,12 +5,15 @@ import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.Scanner;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class PortScanner {
 	
 	public static void main(String[] args) {
 		
 		String host = "localhost";
+		ExecutorService executorService = Executors.newCachedThreadPool();
 		if (args.length > 0) {
 			host = args[0];
 		}
@@ -21,28 +24,45 @@ public class PortScanner {
 		try {
 			InetAddress inetAddress = InetAddress.getByName(host);
 			for (int i = 1; i < port_max; i++) {
-				Socket socket = null;
-				try {
-					 socket = new Socket(inetAddress, i);
-					System.out.println("There is a server on port " + i + " of " + host);
-				} catch (Exception e) {
-					// TODO: handle exception
-					continue;
-				} finally {
-					if (socket != null) {
-						try {
-							socket.close();
-						} catch (IOException e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
-						}
-					}
-				}
-				
+				executorService.execute(new Connect(inetAddress, i));
 			}
+			executorService.shutdown();
 		} catch (UnknownHostException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
+	}
+	
+	public static class Connect implements Runnable {
+
+		private int port;
+		private InetAddress host;
+		
+		public Connect(InetAddress host, int port) {
+			// TODO Auto-generated constructor stub
+			this.host = host;
+			this.port = port;
+		}
+		@Override
+		public void run() {
+			// TODO Auto-generated method stub
+			Socket socket = null;
+			try {
+				 socket = new Socket(host, port);
+				System.out.println("There is a server on port " + port + " of " + host);
+			} catch (Exception e) {
+				// TODO: handle exception
+			} finally {
+				if (socket != null) {
+					try {
+						socket.close();
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+			}
+		}
+		
 	}
 }
